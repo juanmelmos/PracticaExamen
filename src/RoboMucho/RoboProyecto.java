d371360d6b0e979140ba23ddefc62ffe5f10038a
Juan Melguizo <juamelmos@alu.edu.gva.es>
2023-02-08 08:37:01 +0100

    For con 10 Hola Alex

---
+++ src/RoboMucho/RoboProyecto.java
@@ -0,0 +1,17 @@
+package RoboMucho;
+
+public class RoboProyecto {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hola Alex");
+		}
+		
+		
+		
+
+	}
+
+}
